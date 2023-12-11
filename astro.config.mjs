06eaeb2a5ba66e05e83d5dcddfeb6c0b262e25aa
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-12-11 10:59:21 +0100

    load netlify adapter correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,10 +31,5 @@ export default defineConfig({
     },
   },
   output: 'server',
-  adapter:
-    env.NETLIFY === 'TRUE'
-      ? netlify()
-      : node({
-          mode: 'standalone',
-        }),
+  adapter: netlify(),
 })
