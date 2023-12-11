a2509f57d43e9cbbff475a25bd0d4e37d4731f07
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-12-11 10:50:17 +0100

    configure netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,39 @@
-import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
-import tailwind from '@astrojs/tailwind';
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import node from "@astrojs/node";
-const env = loadEnv('', process.cwd(), 'STORYBLOK');
-
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from '@astrojs/tailwind'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import node from '@astrojs/node'
+import netlify from '@astrojs/netlify/functions'
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [storyblok({
-    accessToken: env.STORYBLOK_TOKEN,
-    apiOptions: {
-      region: ''
-    },
-    components: {
-      page: 'storyblok/Page',
-      feature: 'storyblok/Feature',
-      grid: 'storyblok/Grid',
-      teaser: 'storyblok/Teaser'
-    }
-  }), tailwind()],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: '',
+      },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind(),
+  ],
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true
-    }
+      https: true,
+    },
   },
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
-});
+  output: 'server',
+  adapter: env.NETLIFY
+    ? netlify()
+    : node({
+        mode: 'standalone',
+      }),
+})
